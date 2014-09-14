444804d65d56f2e5de284ea8558c3c3fdabefc86
spam0use <spam0use@yahoo.com>
2014-09-14 14:41:52 -0400

    module 2 complete

--- ap2.py
+++ ap2.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python 
 
+import pdb
 import pr2
 import ap1
 import random
 import numpy as np
+import pandas as pd
 import cPickle as pickle
 import os
 import matplotlib.pyplot as plt
+import time
 
 def erugraph(nodes,prob):
     ''' 
@@ -69,19 +72,151 @@ def resilience_plot_random(thresh=True,show=True):
     plt.xlabel('number nodes deleted')
     plt.title('resilience of empirical and simulated computer networks')
     if show:
-		plt.show()
+        plt.show()
     else:
-		plt.savefig('ap2f1.png')
+        plt.savefig('ap2f1.png')
 
 def samplemaxdegree(graph):
-	""" utility fn, return one node with maximum degree """
-	degs=ap1.compute_in_degrees(graph)
-	maxdeg=max(degs.values())
-	maxnodes=[k for k,v in degs.items() if v==maxdeg]
-	attack=np.random.choice(a=maxnodes,size=1)[0]
-	return attack
+    """ utility fn, return one node with maximum degree """
+    degs=ap1.compute_in_degrees(graph)
+    maxdeg=max(degs.values())
+    maxnodes=[k for k,v in degs.items() if v==maxdeg]
+    attack=np.random.choice(a=maxnodes,size=1)[0]
+    return attack
 
-def first_targeted_order(ugraph):
-	""" version of tergeted_order.  attacks graph by removing largest-degree node at each step"""
-	graph=pr2.copy_graph(ugraph)
-	cc=pr2.largest_cc(graph)
+def targeted_order(ugraph):
+    """
+    Compute a targeted attack order consisting
+    of nodes of maximal degree
+    
+    Returns:
+    A list of nodes
+    """
+    # copy the graph
+    new_graph = pr2.copy_graph(ugraph)
+    
+    order = []    
+    while len(new_graph) > 0:
+        max_degree = -1
+        for node in new_graph:
+            if len(new_graph[node]) > max_degree:
+                max_degree = len(new_graph[node])
+                max_degree_node = node
+        
+        neighbors = new_graph[max_degree_node]
+        new_graph.pop(max_degree_node)
+        for neighbor in neighbors:
+            new_graph[neighbor].remove(max_degree_node)
+
+        order.append(max_degree_node)
+    return order
+    
+                    
+def fast_targeted_order(ugraph):
+    """ putatively faster version of tergeted_order.  attacks graph by removing largest-degree node at each step"""
+    graph=pr2.copy_graph(ugraph)
+    n=len(graph)
+    degreeset=[[] for i in range(n)]
+    for nod in graph.keys():
+        degreeset[len(graph[nod])]+=[nod]
+    attack=[]
+    for k in xrange(n-1,-1,-1):
+        while len(degreeset[k])>0:
+            node=np.random.choice(degreeset[k])
+            degreeset[k].remove(node)
+            neighbors=graph[node]
+            for neigh in neighbors:
+                d=[ind for ind,v in enumerate(degreeset) if neigh in v][0]
+                degreeset[d].remove(neigh)
+                degreeset[d-1]+=[neigh]
+            attack.append(node)
+            pr2.delete_node(graph,node)
+    return attack            
+
+def timetargeted():
+    ns=range(10,1000,10)
+    #graphs=[upagraph(n,5) for n in ns]
+    df=pd.DataFrame(index=ns,columns=['targeted_order','fast_targeted_order'])
+    for i in range(len(ns)):
+        times=[]
+        for j in range(5):
+            gr=upagraph(ns[i],5)
+            tm=time.time()
+            devnull=targeted_order(gr)
+            times.append(time.time()-tm)
+        df.iloc[i,0]=np.mean(times)
+        times=[]
+        for j in range(5):
+            gr=upagraph(ns[i],5)
+            tm=time.time()
+            devnull=fast_targeted_order(gr)
+            times.append(time.time()-tm)
+        df.iloc[i,1]=np.mean(times)
+    return df
+    
+def plottimetargeted(df):
+    plt.plot(df.index,df.iloc[:,0],label='targeted_order',color='red')
+    plt.plot(df.index,df.iloc[:,1],label='fast_targeted_order',color='blue')
+    plt.xlabel('n')
+    plt.ylabel('avg time, s')
+    plt.legend(loc='upper left')
+    plt.title('execution time of targeted order functions')
+    plt.show()
+    plt.close()
+    
+    plt.plot(df.index,df.iloc[:,0]**0.5,label='targeted_order',color='red')
+    plt.plot(df.index,df.iloc[:,1]**0.5,label='fast_targeted_order',color='blue')
+    plt.plot(df.index,(df.iloc[:,0]*7)**0.5,label='targeted_order * 7',color='red',linestyle='--')
+    plt.xlabel('n')
+    plt.ylabel('sqrt avg time, s')
+    plt.legend(loc='upper left')
+    plt.title('sqrt of execution time of targeted order functions')
+    plt.show()
+    plt.close()
+
+def plottargetedresilience(show=True,thresh=True):
+    plt.plot(pr2.compute_resilience(net,targeted_order(net)),label='empirical',color='blue')
+    plt.plot(pr2.compute_resilience(simneter,targeted_order(simneter)),label='erdos-renyi, p=0.0017',color='red')
+    plt.plot(pr2.compute_resilience(simnetupa,targeted_order(simnetupa)),label='preferenital attachment, m=2.35',color='green')
+    plt.xlabel('number nodes deleted')
+    plt.ylabel('largest connected component size')
+    plt.title('resilience of empirical and simulated computer networks\n to targeted attacks')
+    if thresh:
+        plt.plot(np.arange(1347,1,-1)*.75,color='gray',linestyle='--',label='resilience threshold')
+    plt.legend(loc='upper right')
+    if show:
+        plt.show()
+    else:
+        plt.savefig('ap2f3.png')
+    plt.close()
+
+# def fast_targeted_order(ugraph):
+    # """ faster version of tergeted_order.  attacks graph by removing largest-degree node at each step"""
+    # graph=pr2.copy_graph(ugraph)
+    # degreeset={}
+    # for nod in graph.keys():
+        # degreeset[len(graph[nod])]=degreeset.get(len(graph[nod]),[])+[nod]
+    # attack=[]
+    # for k in xrange(max(degreeset.keys()),-1,-1):
+        # while k in degreeset and len(degreeset[k])>0:
+            # node=np.random.choice(degreeset[k])
+            # attack.append(node)
+            # neighbors=graph[node]
+            # pr2.delete_node(graph,node)
+            # degreeset[k].remove(node)
+            # for neigh in neighbors:
+                # neighdeg=[ind for ind,v in degreeset.items() if neigh in v][0]
+                # degreeset[neighdeg].remove(neigh)
+                # degreeset[neighdeg-1]=degreeset.get(neighdeg-1,[])+[neigh]
+    # return attack          
+    
+#generate or load graphs
+if(not os.path.isfile('nets.pkl')):
+    net=pr2.load_graph(pr2.NETWORK_URL) #3112 edges
+    simneter=erugraph(1347,.0017)    #3092 edges
+    simnetupa=upagraph(1347,2)      #2683 edges
+    pickle.dump([net,simneter,simnetupa],open('nets.pkl','wb'))
+else:
+    net,simneter,simnetupa=pickle.load(open('nets.pkl','rb'))
+    
+    
